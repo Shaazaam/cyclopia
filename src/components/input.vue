2f110cc073e6e0a1c115a9346ff0d8fdb3b03cfb
Slaggy Joe <baf5042@gmail.com>
2023-05-10 08:10:37 -0400

    - Create an input component to replace <input> elements, this is mostly to encapsulate validation/error behavior

---
+++ src/components/input.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    :value="modelValue"
+    :type="type"
+    :name="name"
+    class="form-control"
+    :class="{'is-invalid': functions.isNotEmpty(errors)}"
+    :placeholder="placeholder"
+    :autocomplete="autocomplete"
+    @input="$emit('update:modelEvent', $event.target.value)"
+  />
+  <div class="invalid-feedback">{{functions.isNotEmpty(errors) ? errors[name] : ''}}</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      modelValue: {
+        type: [String, Number],
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      placeholder: {
+        type: String,
+      },
+      autocomplete: {
+        type: String,
+        default: 'on',
+      },
+    },
+    emits: ['update:modelValue'],
+  }
+</script>
